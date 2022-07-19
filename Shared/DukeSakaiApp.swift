84e60c5f5e177b8c533f232762ae7f56698a083b
Luke Redmore <lr197@duke.edu>
2022-07-18 21:54:35 -0400

    Initial Commit

---
+++ Shared/DukeSakaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DukeSakaiApp.swift
+//  Shared
+//
+//  Created by Luke Redmore on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DukeSakaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
